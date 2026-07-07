eab5e461c558c90482ceda32489d1a42da421512
Abhilash Kommana <abhikommana@gmail.com>
2026-07-07 11:36:23 +0530

    Update import paths for connectDB and Student

--- app/api/students/[id]/route.js
+++ app/api/students/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import connectDB from '../../../lib/mongodb';
-import Student from '../../../models/Student';
+import connectDB from '../../../../lib/mongodb';
+import Student from '../../../../models/Student';
 
 export async function GET(request, { params }) {
   try {
