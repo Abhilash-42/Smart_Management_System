59121da3cf6e26c9a5ad17e2155e8c89f7bd3bf9
Abhilash Kommana <abhikommana@gmail.com>
2026-07-07 11:37:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  const nextConfig = {}
+
+module.exports = nextConfig
   images: {
     domains: ['localhost'],
   },
